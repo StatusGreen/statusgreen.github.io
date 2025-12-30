030dc70363b605541fd8257d181b31b69152b3b9
biannetta <benjamin.iannetta@gmail.com>
2025-12-29 23:55:49 -0500

    Initial Updates to SG Website

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,8 @@
-/** @type {import('vite').UserConfig} */
-export default {
-}
+import { defineConfig } from "vite"
+import tailwindcss from "@tailwindcss/vite"
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+  ]
+})
